f7839f4f7c7b607a7a02241f74df58a95a2e606f
Shashikant Tanti <88486204+Shashikant-15@users.noreply.github.com>
2021-12-29 16:36:59 +0530

    first commit

--- src/Adv_13/Multi_Thread_Implement.java
+++ src/Adv_13/Multi_Thread_Implement.java
@@ -12,26 +12,27 @@ public class Multi_Thread_Implement {
         th.start();
     }
 }
-class RandomNumber extends Thread {
+class RandomNumber extends Thread {   // extended class to Thread
+    
     public void run() {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner=new Scanner(System.in);   // taking input from user
         System.out.println("Please enter the specific number up to  you want to generate square and cube of Random number");
         int numOfRandom= scanner.nextInt();
         Random random = new Random();
-        for(int i=0;i<numOfRandom;i++){
+        for(int i = 0; i < numOfRandom; i++){
             int randomNumber = random.nextInt(100);
             System.out.print("Generating");
-            for (int j =0; j<5; j++){
-//                we are printing . every half second for waiting
+            for (int j = 0; j < 5; j++){
+           //   we are printing . every half second for waiting
                 System.out.print(".");
-                try {
+                try {                       // try & catch block used 
                     Thread.sleep(500);
                 }catch (InterruptedException ex){
                     ex.printStackTrace();
                 }
             }
             System.out.println();
-            if(randomNumber % 2==0){
+            if(randomNumber % 2 == 0){
                 SquareThread squareThread=new SquareThread(randomNumber);
                 squareThread.start();
             }else{
